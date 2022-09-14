6a942aa1dd1c2a0c84792a75a8acfb504c2e94aa
= <m4th3usm3d3ir0s@gmail.com>
2022-09-14 14:38:39 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -9,5 +9,13 @@ app.get('/product/:id/:user', (request, response) => {
     For user: ${user}. `)
 })
 
+app.get('/users/', (request, response) => {
+  const { page, limit } = request.query
+  response.send(`
+  Page: ${page}.
+  Limit: ${limit}.
+  `)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
