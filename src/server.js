06bbb71bd280ecac44c0b1d17a4ecff5ae7676f3
= <m4th3usm3d3ir0s@gmail.com>
2022-09-14 15:04:37 -0300

    Project estructure reorganized - Routes folder added

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 
+const routes = require('./routes')
+
 const app = express()
 app.use(express.json())
 
-app.post('/users/', (request, response) => {
-  const { name, email, password } = request.body
-  /* response.json(`User: ${name} - Email: ${email} - Password: ${password}`) Devolve em HTML (Não é tão conveniente) */
-  response.json({ name, email, password }) // Devolve em formato JSON (Objeto) padrão mais conveniente em resposta de API
-})
+app.use(routes)
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
