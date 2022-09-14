6a8e37ed4de47e7b5800fc98ac1b60398f46cef6
= <m4th3usm3d3ir0s@gmail.com>
2022-09-14 14:20:32 -0300

    Route Params

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/message', (request, response) => {
-  response.send('Hello, world!')
+app.get('/product/:id/:user', (request, response) => {
+  const { id, user } = request.params
+  response.send(`
+  Product id: ${id}.
+  For user: ${user}.
+  `)
 })
 
 const PORT = 3333
