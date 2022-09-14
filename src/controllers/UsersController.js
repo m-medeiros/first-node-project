a7446579a972712521a17505c234521281f81650
= <m4th3usm3d3ir0s@gmail.com>
2022-09-14 15:09:04 -0300

    Created controller (NYI)

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+class UsersController {
+  /* 
+  Um controller pode ter no máximo 5 métodos:
+  index - GET para listar vários registros.
+  show - GET para exibir um registro específico.
+  create - POST para criar um registro.
+  update - PUT para atualizar um registro.
+  delete - DELETE para remover um registro.
+  */
+}
+
+module.exports = UsersController
