cfb9efbde187b7df0d6f4459cef1d6d6a0ee8ca5
= <m4th3usm3d3ir0s@gmail.com>
2022-09-14 15:42:53 -0300

    HTTP codes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,8 @@
 class UsersController {
-  /* 
-  Um controller pode ter no máximo 5 métodos:
-  index - GET para listar vários registros.
-  show - GET para exibir um registro específico.
-  create - POST para criar um registro.
-  update - PUT para atualizar um registro.
-  delete - DELETE para remover um registro.
-  */
+  create(request, response) {
+    const { name, email, password } = request.body
+    response.status(201).json({ name, email, password })
+  }
 }
 
 module.exports = UsersController
