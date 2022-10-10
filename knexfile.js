dcfb040941e181f85c3239d7cabdaa317c572a40
= <m4th3usm3d3ir0s@gmail.com>
2022-10-10 14:27:36 -0300

    Fixed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db");
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
-    useNullAsDefault: true;
+    useNullAsDefault: true
   }
 }
