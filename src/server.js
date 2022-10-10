bda9e06cbfe8f89f174e25123c7598b672de7018
= <m4th3usm3d3ir0s@gmail.com>
2022-10-10 13:15:05 -0300

    Added knex

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(routes)
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
-      status: 'error',
+      status: 'errors',
       message: error.message
     })
   }
